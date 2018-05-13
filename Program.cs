35486f1d190869ef7f9ab804da8ebe753719035d
meogol <avs99.98@gmail.com>
2018-05-14 00:45:02 +0300

    s

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using ConsoleApp1;
 
 namespace ConsoleApp3
