d93ce5fcedbc980b6b0d82dfb1886707d57f6744
meogol <avs99.98@gmail.com>
2018-09-20 20:05:14 +0300

    9) Вывести топ 3 менеджеров на чьих складах большее количество товаров.
    9) Вывести топ 3 менеджеров на чьих складах большее количество товаров.

---
+++ SQLQuery13.sql
@@ -0,0 +1,11 @@
+select (select FIO
+				from manager
+				where ID=manager_stock.manager_ID) as FIO,
+(SELECT  sum(count)
+from stock_product
+where stock_product.stock_ID=manager_stock.stock_ID
+group by stock_ID
+) as count
+from manager_stock order by 2 DESC
+SET ROWCOUNT 3
+
