81b2233d900f3ed3d45c9f5d9c88f9fb870ead7e
meogol <avs99.98@gmail.com>
2018-09-20 21:11:58 +0300

    8) Запрос выводит менеджеров, чьи склады обслуживюат более одного магазина.
    8) Запрос выводит менеджеров, чьи склады обслуживюат более одного магазина.

---
+++ SQLQuery14.sql
@@ -0,0 +1,9 @@
+select
+	(SELECT count(stock_ID)
+	from stock_magazine
+	group by stock_ID
+	having stock_ID=manager_stock.stock_ID and count(stock_ID)>1) as magazine,
+	(select FIO
+	from manager
+	where ID=manager_stock.manager_ID)
+from manager_stock
