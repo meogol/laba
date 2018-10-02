995605a972c2cfe119985315d39ae7cbbbadd666
meogol <avs99.98@gmail.com>
2018-10-02 21:37:01 +0300

    Add files via upload

--- SQLQuery13.sql
+++ SQLQuery13.sql
@@ -1,14 +1,9 @@
-﻿/*	9) Вывести топ 3 менеджеров на чьих складах большего количества товаров.*/
+﻿/*9) Вывести топ 3 менеджеров на чьих складах большего количества товаров.*/
 
-select FIO, Count
-from (
-	select TOP(3) sum(count) as Count,stock_ID
-	from stock_product
-	group by stock_ID) as t1
-inner join (
-	select manager.FIO, manager_stock.stock_ID
-	from manager 
-	inner join manager_stock on manager.ID=manager_stock.manager_ID
-) as t2 on t1.stock_ID=t2.stock_ID
-
+select top(3) FIO, sum(Count) as SumCount
+from stock_product
+inner join manager_stock on manager_stock.stock_ID=stock_product.stock_ID
+inner join manager on manager.ID=manager_stock.manager_ID
+group by FIO
+order by sum(Count) DESC
 
--- SQLQuery30.sql
+++ SQLQuery30.sql
@@ -1,12 +1,8 @@
 ﻿/*10) Увеличить на 1 количество всех товаров на складах с именем товара «фисташка» и
 которые обслуживают магазины с именем «мир пустоты».*/
-
 update stock_product
 set count=count+1
-from (select product.ID as pID, t1.stock_ID as sID
-	from product
-	inner join (select stock_ID, magazine_ID
-		from magazine 
-		inner join stock_magazine on magazine.name='продукты' and magazine.ID=stock_magazine.magazine_ID
-		) as t1 on product.name='хлеб') as t2
-where stock_product.product_ID=t2.pID and stock_product.stock_ID=t2.sID
+from product
+inner join magazine on magazine.name='продукты' and product.name='хлеб'
+inner join stock_magazine on stock_magazine.magazine_ID= magazine.ID
+where stock_product.stock_ID=stock_magazine.stock_ID and stock_product.product_ID=product.ID
