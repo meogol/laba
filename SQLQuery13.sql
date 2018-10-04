d88620942ecb5fc3b67b681f17b5baffd4c5bcba
meogol <avs99.98@gmail.com>
2018-10-04 22:15:18 +0300

    Add files via upload

--- SQLQuery13.sql
+++ SQLQuery13.sql
@@ -1,9 +1,9 @@
 ﻿/*9) Вывести топ 3 менеджеров на чьих складах большего количества товаров.*/
 
+
 select top(3) FIO, sum(Count) as SumCount
 from stock_product
 inner join manager_stock on manager_stock.stock_ID=stock_product.stock_ID
 inner join manager on manager.ID=manager_stock.manager_ID
-group by FIO
+group by FIO, ID
 order by sum(Count) DESC
-
--- SQLQuery30.sql
+++ SQLQuery30.sql
@@ -1,8 +1,10 @@
-﻿/*10) Увеличить на 1 количество всех товаров на складах с именем товара «фисташка» и
+﻿	/*10) Увеличить на 1 количество всех товаров на складах с именем товара «фисташка» и
 которые обслуживают магазины с именем «мир пустоты».*/
 update stock_product
 set count=count+1
-from product
-inner join magazine on magazine.name='продукты' and product.name='хлеб'
-inner join stock_magazine on stock_magazine.magazine_ID= magazine.ID
-where stock_product.stock_ID=stock_magazine.stock_ID and stock_product.product_ID=product.ID
+from stock_product
+inner join product on product.name='хлеб' and product.ID=stock_product.product_ID
+inner join stock_magazine on stock_product.stock_ID=stock_magazine.stock_ID and stock_magazine.magazine_ID=(
+		select ID
+		from magazine
+		where magazine.name='продукты')
