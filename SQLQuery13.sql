6009fe0fa269cff0a75b33d55c1ba3d2e647d062
meogol <avs99.98@gmail.com>
2018-10-06 00:23:38 +0300

    Add files via upload

--- SQLQuery13.sql
+++ SQLQuery13.sql
@@ -1,9 +1,9 @@
 ﻿/*9) Вывести топ 3 менеджеров на чьих складах большего количества товаров.*/
-
-
-select top(3) FIO, sum(Count) as SumCount
-from stock_product
-inner join manager_stock on manager_stock.stock_ID=stock_product.stock_ID
-inner join manager on manager.ID=manager_stock.manager_ID
-group by FIO, ID
-order by sum(Count) DESC
+SELECT top(3) FIO, stock
+from manager
+inner join (
+	select sum(count) as stock, manager_stock.manager_ID
+	from stock_product
+	inner join manager_stock on stock_product.stock_ID= manager_stock.stock_ID
+	group by manager_stock.manager_ID) as t1 on manager.ID=t1.manager_ID
+order by stock DESC
